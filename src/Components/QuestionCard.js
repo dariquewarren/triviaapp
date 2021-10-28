6f3a07b5b157a98d1c6507f51cc4433ab6df4a66
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-10-28 16:14:54 -0400

    added a form with  true and false checkboxes. changed real guess to userguess. added states for each checkbx

--- src/Components/QuestionCard.js
+++ src/Components/QuestionCard.js
@@ -4,10 +4,14 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 function QuestionCard(props) {
    
-   const [realGuess, changeRealGuess] = useState('no guess')
-   
+   const [userGuess, changeUserGuess] = useState('NO GUESS SELECTED')
+   const [guessTrue, changeGuessTrue] = useState(false)
+   const [guessFalse, changeGuessFalse] = useState(false)
 
-   
+
+   const handleUserGuess=(guess)=>{
+       console.log
+   }
 
   // create  an object with the following Keys
   // Question, correctAnswer, guess, isGuessCorrect
@@ -20,7 +24,7 @@ function QuestionCard(props) {
 //   }
   
     return (
-        <div>
+        <div >
         
 
         <header> 
@@ -32,6 +36,27 @@ function QuestionCard(props) {
             qCard Data
             </button>
             </header>
+            <Card>
+            <Card.Header>{props.question}</Card.Header>
+            <h3>Your Guess: {userGuess}</h3>
+            <Form 
+            onSubmit={(e)=>{
+                e.preventDefault()
+                console.log('form e target', e.target.trueCheckBox)
+            }
+            }
+            >
+            <Form.Check onClick={(e)=>{
+                console.log('true checkbox target value',e.target.value)
+                changeUserGuess(e.target.value)
+            }} id='trueCheckbox' type='checkbox' label='True' value='True'/>
+            <Form.Check onClick={(e)=>{
+                console.log('true checkbox target value',e.target.value)
+                changeUserGuess(e.target.value)
+            }} style={(userGuess === 'False' ? {display: 'block'} : {display: 'none'} )} type='checkbox' label='False' value='False'/>
+            <Button type='submit'> Submit/ data test</Button>
+            </Form>
+            </Card>
         </div>
     )
 }
