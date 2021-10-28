524c7b743c739fba47290f2be568e7b859928b65
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-10-28 15:36:51 -0400

    stripped down appjs and quizpage to simplify/refactor/ do better/ write cldaner more effective code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import QuestionCard from './Components/QuestionCard';
 import QuizPage from './Components/QuizPage';
 function App() {
-const [quizData, setQuizData] = useState([])
+const [quizData, setQuizData] = useState()
    const getTriviaData =()=>{
      fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean').then((response)=>{
       return response.json()
      }).then((data)=>{
        const realData = data.results
+       setQuizData(data.results)
       console.log('results',data.results)
+      return realData
       
-      setQuizData(realData)
-      console.log('quizData',quizData)
      })
    }
+
+useEffect(()=>{
+
+},[quizData])
+
   return (
     <div className="App">
    <header>
    <button
-   onClick={()=>{
-     
-    getTriviaData()
+  onClick={()=>{
     
+   getTriviaData()
+   
+ }}
+  > start quiz/ get data</button>
+  <button
+  onClick={()=>{
+    console.log(quizData)
   }}
-   > Test Data Button</button>
+  >
+  log data
+  </button>
    </header>
-<QuizPage quiz={quizData} quizLength={quizData.length}/>
 
+   <QuizPage quiz={quizData} />
     </div>
   );
 }
--- src/Components/QuizPage.js
+++ src/Components/QuizPage.js
@@ -1,103 +1,11 @@
 import React, { useState, useEffect} from 'react'
+import Button from 'react-bootstrap/Button'
 import QuestionCard from './QuestionCard'
 function QuizPage(props) {
-
-const [quizResponses, changeQuizResponses] = useState([])
-
-const [questionIndexNumber, modifyQuestionNumber] = useState(0)
-
-const changeQuestionNumber = (direction)=>{
-    var newQuestionNumber = questionIndexNumber
-
-if(direction === 'add' && questionIndexNumber <= props.quiz.length ){
-  return  modifyQuestionNumber(newQuestionNumber + 1)
-
-} else if(direction === 'subtract'  && questionIndexNumber > -1){
- return   modifyQuestionNumber(newQuestionNumber - 1)
-
-}else{
- return   alert('error')
-}
-
-
-}
-
-
-
-
-const currentQuestion = (theNumber) =>{
-    return props.quiz.filter((f)=>{
-        return props.quiz.indexOf(f) === theNumber
-    })
-
-    
-}
-
-
-const addQuizObject = (quizObject)=>{
-const newQuizArray = quizResponses.filter((f)=>f.guess !== 'no guess')
-newQuizArray.push(quizObject)
-changeQuizResponses(newQuizArray)
-console.log(quizResponses)
-}
 
 
     return  (
         <div>
-            
-        
-        <button
-            onClick={()=>{
-                console.log('current Question', currentQuestion(questionIndexNumber))
-
-                console.log('props', props)
-                console.log('quiz Array', quizResponses)
-                
-            }}
-            >quiz page fns test</button>
-
-            <Button onClick={()=>{
-                if(props.questionNumber < 1 ){
-                    return alert('too far back')
-                } else{
-                    props.changeQuestionNumber('subtract')
-                }
-                
-                console.log('quiz length', props.quizLength)
-                console.log('question Number', props.questionNumber)
-    
-            }}>
-                previous
-                </Button>
-    
-    
-    
-            <Button onClick={()=>{
-                props.changeQuestionNumber('add')
-                console.log('quiz length', props.quizLength)
-                console.log('question Number', props.questionNumber)
-            }}>
-                next
-                </Button>
-
-{
-
-  currentQuestion(questionIndexNumber).map((m)=>{
-    
-    return(
-        <QuestionCard 
-        key={props.quiz.indexOf(m)}
-        quizLength={props.quiz.length}
-        changeQuestionNumber={changeQuestionNumber}
-        questionNumber={questionIndexNumber}
-          
-         question={m.question} 
-         correctAnswer={m.correct_answer} 
-         addQuizObject={addQuizObject} />
-    )
-})
-    
-}
       
         </div> 
     ) 
