d33f3aaeb07821e1059137d2b85f3d229cf246a1
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-10-31 15:06:08 -0400

    added a note to myself about features I want to add later

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const [quizAnswers, setQuizAnswers] = useState([])
 const [questionsAnswered, addQuestionsAnswered] = useState(0)
 const [showResultsPage, toggleShowResultsPage] = useState(false)
 
-   
+// at some point I want different quiz buttons to select different quizzes based on params available like difficulty, amount of questions, category and whether or not the options are strings or boolean
+
    const getTriviaData =()=>{
      fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean').then((response)=>{
       return response.json()
