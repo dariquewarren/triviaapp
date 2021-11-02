c73467e6d309eff3af0a689aa09659269ef183a9
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-02 16:32:34 -0400

    setup conditional toggling of showing type selection

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -6,18 +6,32 @@ import Badge from 'react-bootstrap/Badge'
 // OR conditionally render necessary props into the same card (because styling is till tedious for me lol)
 
 function MainHeader(props) {
+// just for the main fetch call
 const [qAmount, setQAmount] = useState(10)
 const [qCategory, setQCategory] = useState(9)
 const [qDifficulty, setQDifficulty] = useState('easy')
 const [qType, setQType] = useState('boolean')
-const [showCategory, toggleShowCategory] = useState(false)
+// just for the component ui display
+const [categoryName, setCategoryName] = useState('General Knowledge')
 const [categoryList, setCategoryList] = useState([])
+// show hide quiz parameter selection components
+const [showCategory, toggleShowCategory] = useState(false)
+const [showAmount, toggleShowAmount] = useState(false)
+const [showDifficulty, toggleShowDifficulty] = useState(false)
+const [showType, toggleShowType] = useState(false)
+
 
     // set states for the following to be used with props.gettriviadata function
         //amount, category,difficulty,type
+
+        // consider putting the buttons in the header behond a settings menu
     return (
         <div>
             <h1>Main header</h1>
+            <row>
+             # Of Questions{qAmount} Category: {categoryName} Difficulty:{qDifficulty} Type: {qType}
+            </row>
+            <br></br>
             <row>
             <button
             onClick={()=>{
@@ -30,6 +44,9 @@ const [categoryList, setCategoryList] = useState([])
            }}
             >{(props.quizData.length < 1) ? 'start quiz' : 'new quiz'  }  </button>
             
+            <button># of Questions</button>
+
+
             <button onClick={()=>{
                 fetch('https://opentdb.com/api_category.php').then((response)=>{
                     return response.json()
@@ -42,30 +59,63 @@ const [categoryList, setCategoryList] = useState([])
 
                 })
             
-            }}>{(showCategory)? 'Hide Categories': 'Show Categories'}</button>
+            }}>
+            {(showCategory)? 'Hide Categories': 'Show Categories'}
+            </button>       
+
+            <button
+            onClick={()=>{
+                toggleShowType(!showType)
+            }}
+            >Select Type</button>
+
             {
-                
-            }
-           {
-               (showCategory) 
-               ?
-               categoryList.trivia_categories.map((m)=>{
-                return(
-                    <button key={m.id} value={m.id}> {m.name}</button>
-                )
-            })
+                (showType)
+                ?
+                <div>
+                <button
+                value={'boolean'}
+                onClick={(e)=>{
+                    setQType(e.target.value)
+                }}
+                >True or False</button>
+                <button
+                value={'multiple'}
+                onClick={(e)=>{
+                    setQType(e.target.value)
+                }}
+                >Multiple Choice</button>
+
+                </div>
                 :
-               <p></p>
-           }
-
-            <button>
-           
-
-            # of Questions</button>
-            <button>multipleChoice/boolean</button>
+                <p></p>
+            }
             <button>Difficulty</button>
             
             </row>
+            {
+                (showCategory) 
+                ?
+                 <div style={{border: '3px solid black'}}>
+                  <h2> Select A Category</h2>
+                  { categoryList.trivia_categories.map((m)=>{
+                     return(
+                        
+                         <button 
+                         key={m.id} value={m.id} 
+                         onClick={(e)=>{
+                             e.preventDefault()
+                             console.log('name, value',e.target.value, m.name)
+                         }}
+                         
+                         > {m.name}</button>
+                         
+                     )
+                 })
+                   }
+                 </div>:
+                <p></p>
+            }
         </div>
     )
 }
